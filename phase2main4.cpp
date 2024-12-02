dd3c925b6a3fd2a481cca9b0ca80dbfd646b2cc5
AAzk281 <AZAHAR.KHAN49@qmail.cuny.edu>
2024-12-02 11:38:19 -0500

    Update phase2main4.cpp

--- phase2main4.cpp
+++ phase2main4.cpp
@@ -10,7 +10,15 @@ using namespace std;
                 }
                 return minIdx;
         }
-
+        int getMax(int h[], int cap) {
+                int maxVal = h[0];
+                for (int i = 1; i < cap; i++){
+                        if (h[i] > maxVal) {
+                                maxVal = h[i];
+                        }
+                }
+                return maxVal;
+         }
         int main (){
                 int h[6];
                 cout << "Please enter six non-negative integers." << endl;
@@ -21,9 +29,9 @@ using namespace std;
                 int low = 0, high = 5;
                 int idx = minIndex(h,low, high);
                 cout << "The index of the smallest integer in the subarray is " << idx << endl;
-        }
-~                                                                                                                             
-~                                                                                                                             
-~                                                                                                                             
-~                                                                                                                             
-~              
+                
+                int maxInteger = getMax(h, 6);
+                cout << "The maximum Integer in the array is: " << maxInteger << endl;
+
+                return 0;
+                }
